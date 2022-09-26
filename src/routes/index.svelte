01a7e1b757dbe76cc1b3cfb9bf331028f1205251
Chevyself <chevyself@gmail.com>
2022-09-26 18:22:23 -0500

    Add Spotify music embed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,6 @@
 	<title>Riptide</title>
 </svelte:head>
 
-
-
+<!-- Embed Spotify track -->
+<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3JvrhDOgAt6p7K8mDyZwRd?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 
