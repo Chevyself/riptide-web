48f9a001fa69397e999999595f919dc175d75bca
Chevyself <chevyself@gmail.com>
2022-09-26 22:17:17 -0500

    Add placeholder background

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,37 @@
 </script>
 
 <style lang="scss">
+.center {
+	display: grid;
+	place-items: center;
+}
+#bg {
+  height: 100%;
+  width: 100%;
+  
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+section {
+  height: 100%;
+  width: 100%;
+}
 </style>
 
 <svelte:head>
 	<title>Riptide</title>
 </svelte:head>
 
-<!-- Embed Spotify track -->
-<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3JvrhDOgAt6p7K8mDyZwRd?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+<div style="background-image: url('bg.gif'); position: absolute;" id="bg">
+	<section style="background:transparent;" id="landing">
+
+	</section>
+
+	<section id="track" class="center">
+		<!-- Embed Spotify track -->
+		<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3JvrhDOgAt6p7K8mDyZwRd?utm_source=generator&theme=0" height="38%" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+	</section> 
+</div>
 
