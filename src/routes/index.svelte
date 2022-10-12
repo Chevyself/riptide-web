e694f00ff17039f1bfcee0cce4b92c7687fddfd0
Chevy <chevyself@gmail.com>
2022-10-12 12:01:49 -0500

    Add essay

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -63,12 +63,14 @@ section {
 		<section style="background-color: rgb(10, 10, 10); width: 100%" id="essay" class="center">
 			<div>
 				<h1 style="margin-left: 1.5rem; margin-right: 1.5rem;">Ensayo</h1>
-				<p style="margin-top: 1.5rem; margin-left: 1.5rem; margin-right: 1.5rem;">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<p style="margin-top: 1.5rem; margin-left: 1.5rem; margin-right: 1.5rem;">Este es un proyecto de Animación, que pone en imágenes, la interpretación de las líricas de la canción hecha por Vance Joy: Riptide. 
+Es una animación en doces inspirada en la animación independiente contemporánea.
+La letra metafórica de Riptide da la oportunidad de una representación en el dibujo de una manera mucho mas creativa.</p>
 			</div>
 		</section>
 		
 		<section id="extra" class="center">
-			<h1>TODO</h1>
+			<h1></h1>
 		</section>
 		
 		<section id="footer">
